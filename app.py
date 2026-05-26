430d5777b17ee3317deddc63884c4fe388c7e2a4
Danish Raza <genzgameclips@gmail.com>
2026-05-26 08:35:15 +0530

    Add main check to execute app
    Added main check to run the app.

--- app.py
+++ app.py
@@ -487,3 +487,5 @@ if __name__ == '__main__':
     print("Server running on: http://localhost:5000")
     print("=" * 60)
     app.run(debug=True, host='0.0.0.0', port=5000)
+    if __name__ == '__main__':
+    app.run()
